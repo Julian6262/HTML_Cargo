312fe8e7338c35e597e57162d154748d2ffb44c9
Julian6262 <d.baburin62@gmail.com>
2023-07-07 23:26:58 +0300

    slider media

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@ $(function () {
         nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
         prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
     });
+
+    $('.header__btn-menu').on('click', function () {
+        $('.header__menu ul').slideToggle()
+    });
 });
 
 
