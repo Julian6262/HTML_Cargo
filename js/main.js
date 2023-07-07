789d281ba0db812e2d5cfae352db5991106bf9f1
Julian6262 <d.baburin62@gmail.com>
2023-07-07 19:44:13 +0300

    footer finish

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
 $(function () {
-
     $('.slider__inner').slick({
         nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
         prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
     });
-
-    // $('.header__btn-menu').on('click', function () {
-    //     $('.menu ul').slideToggle()
-    // });
-
 });
+
+
+
+
